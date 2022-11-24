7742c16d15a0fb22b441812091c03a0489b0fdee
Alina <AliKochneva@yandex.ru>
2022-11-24 10:01:03 +0700

    Step one

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string [] inputArray = {"one" , "two0" ,  "hello" , "rus", "test", "Developer", "GeekBrains"};
+int newSize = 0;
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+   if (inputArray[i].Length <= 3)
+   {
+    newSize++;
+   } 
+}
+
+string [] newArray = new string[newSize];
+
